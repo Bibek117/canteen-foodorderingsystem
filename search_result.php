a36bf9ee6e46e917ea3c608b8d4ea35f8d412f92
Bibek Angdembe <Bibekangdembay@gmail.com>
2023-01-29 23:17:33 +0545

    bug fixed for login search not showing

--- search_result.php
+++ search_result.php
@@ -1,14 +1,6 @@
 <?php
 session_start();
 include('connection.php');
-if (isset($_GET['search-submit'])) {
-    $query = mysqli_real_escape_string($conn, $_GET['keyword']);
-    $sql = "SELECT * from food_items WHERE availability = 1 && ( food_name LIKE '%$query%' OR food_desc LIKE '%$query%')";
-    $result = mysqli_query($conn, $sql);
-    $queryResult = mysqli_num_rows($result);
-}
-
-
 ?>
 <!DOCTYPE html>
 <html>
@@ -32,12 +24,14 @@ if (isset($_GET['search-submit'])) {
 </head>
 
 <body>
+    
     <?php
     include('./partials/header.php');
     ?>
     <div class="container">
         <h2 class="menu-title">>Today's Menu</h2>
-        <?php if (isset($_SESSION['user_name']))  echo '<h4>>>Welcome ' . $_SESSION["user_name"] . '</h4>';
+        <?php if (isset($_SESSION['user_name']))  echo '<h4>>>Welcome ' . $_SESSION["user_name"] . '</h4>' ?>
+        <?php
         include('./partials/search_form.php');
         ?>
         <div class="order-note">
@@ -45,44 +39,50 @@ if (isset($_GET['search-submit'])) {
         </div> 
          <div class="menu">
         <?php
-        if ($queryResult > 0) {
-            while ( $row = mysqli_fetch_assoc($result)) {
+        if (isset($_GET['search-submit'])) {
+            $query = $_GET['keyword'];
+            $sql = "SELECT * from food_items WHERE availability = 1 && ( food_name LIKE '%$query%' OR food_desc LIKE '%$query%')";
+            $result = mysqli_query($conn,$sql);
+            $queryResult = mysqli_num_rows($result);
+        }
+           if ($queryResult > 0) {
+            while ($row = mysqli_fetch_assoc($result)) {
         ?>
-                        <div class="menu-item">
-                            <div class="menu-item-img">
-                                <img src="images/<?php echo $row['food_image']; ?>" alt="food1">
-                            </div>
-                            <div class="menu-item-details">
-                                <h3><?php echo $row['food_name']; ?></h3>
-                                <p><?php echo $row['food_desc']; ?></p>
-                                <h4>Rs <?php echo $row['food_price']; ?></h4>
-                                <?php if (isset($_SESSION['user_name']) && $_SESSION['loggedin'] == true) { ?>
-                                <div class="myForm">
-                                    <form action="index.php" class="form-contain" onsubmit="return confirm('Are you sure you want to submit this form?');" method="POST">
-                                        <div class="counter">
-                                            <span class="down" onClick='decreaseCount(event, this)'>-</span>
-                                            <input type="text" value="1" name="quantity">
-                                            <span class="up" onClick='increaseCount(event, this)'>+</span>
-                                        </div>
-                                        <input type="hidden" name="food_id" value="<?php echo $row['food_id']; ?>">
-                                        <button type="submit" name="order_submit" class="btn">Order now</button>
-                                    </form>
-                                </div>
-                            <?php } else { ?>
-                                <a href="login.php" style="display:block;" class="btn">Login to order</a>
-                            <?php } ?>
 
+                <div class="menu-item">
+                    <div class="menu-item-img">
+                        <img src="images/<?php echo $row['food_image']; ?>" alt="food1">
+                    </div>
+                    <div class="menu-item-details">
+                        <h3><?php echo $row['food_name']; ?></h3>
+                        <p><?php echo $row['food_desc']; ?></p>
+                        <h4>Rs <?php echo $row['food_price']; ?></h4>
+                        <!-- <button class="btn orderNow">Order now</button> -->
+                        <?php if (isset($_SESSION['user_name']) && $_SESSION['loggedin'] == true) { ?>
+                            <div class="myForm">
+                                <form action="./index.php" class="form-contain"  onsubmit="return confirm('Are you sure you want to place this order?');" method="POST">
+                                    <div class="counter">
+                                        <span class="down" onClick='decreaseCount(event, this)'>-</span>
+                                        <input type="text" value="1" name="quantity">
+                                        <span class="up" onClick='increaseCount(event, this)'>+</span>
+                                    </div>
+                                    <input type="hidden" name="food_id" value="<?php echo $row['food_id']; ?>">
+                                    <button type="submit" name="order_submit" class="btn">Order now</button>
+                                </form>
                             </div>
-                            </div>
-                      
-                   
+                        <?php } else { ?>
+                            <a href="login.php" style="display:block;" class="btn">Login to order</a>
+                        <?php } ?>
+                    </div>
+                </div>
+
         <?php
-              }  }
-         else {
+            }
+        } else {
             echo '<div class="no-menu">
-            <img src="./images/sorry.png" alt="sorry">
-            <h3>Sorry, There is no such  item available right now!!</h3>
-            </div>';
+        <img src="./images/sorry.png" alt="sorry">
+        <h3>Sorry, No food item available right now!!</h3>
+        </div>';
         }
       ?>
     </div> 
